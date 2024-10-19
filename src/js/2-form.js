4c87c2dc6110c12abf07a59ada0c9a2be5e2c50d
Lia <natalimail911@gmail.com>
2024-10-19 16:06:53 +0300

    form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,39 @@
-const feedbackForm = document.querySelector(".feedback-form");
+const feedbackForm = document.querySelector('.feedback-form');
+
+checkInputStart();
+
+feedbackForm.addEventListener('input', onInputChange);
+feedbackForm.addEventListener('submit', onFormSubmit);
+
+function onInputChange() {
+    const formData = {
+        email:(feedbackForm.elements.email.value).trim(),
+        message:(feedbackForm.elements.message.value).trim(),
+    }
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const email = (feedbackForm.elements.email.value).trim();
+    const message = (feedbackForm.elements.message.value).trim();
+
+    if (!email || !message) {
+        alert('Fill please all fields');
+        return;
+    }
+
+    const userInfo = { email: email, message: message };
+    console.log(userInfo);
+    
+    feedbackForm.reset();
+    localStorage.clear();
+}
+
+function checkInputStart() {
+    const localInfo = JSON.parse(localStorage.getItem("feedback-form-state")) ?? {};
+
+     feedbackForm.elements.email.value = localInfo.email || '';  
+     feedbackForm.elements.message.value = localInfo.message || ''; 
+}
